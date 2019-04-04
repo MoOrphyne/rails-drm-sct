338cb3f07b27ef2ae8e831d694f9fa72f7a5e12e
romain sanson <romainsanson.gm@gmail.com>
2019-04-04 19:38:06 +0200

    homepage clean

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @last_packs = Pack.last(4)
     @slots = 500 - User.where(subscriber: true).count
   end
 end
