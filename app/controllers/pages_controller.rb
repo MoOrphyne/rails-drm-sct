2e67718da23ea9dd8713fbed2c22c6121b600120
romain sanson <romainsanson.gm@gmail.com>
2019-04-04 20:06:39 +0200

    pricing page css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @last_packs = Pack.last(4)
+    @last_packs = Pack.order('created_at DESC').first(4)
     @slots = 500 - User.where(subscriber: true).count
   end
 end
