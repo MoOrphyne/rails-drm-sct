c93f0315a101a0f7e5e779850c07518c448c813d
romain sanson <romainsanson.gm@gmail.com>
2019-04-04 13:44:24 +0200

    page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @slots = 500 - User.where(subscriber: true).count
   end
 end
