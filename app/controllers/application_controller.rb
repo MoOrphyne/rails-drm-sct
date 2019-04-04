2f3cd7466fe926ec8e545e927a5a170d46a7d74e
romain sanson <romainsanson.gm@gmail.com>
2019-04-04 22:51:13 +0200

    front

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    update_attrs = [:email, :photo, :photo_cache, :password, :password_confirmation, :current_password]
+    update_attrs = [:username, :email, :photo, :photo_cache, :password, :password_confirmation, :current_password]
     devise_parameter_sanitizer.permit :sign_up, keys: update_attrs
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
   end
