649a56217d5802c792e7a016310cf13df6f0d9d9
delphine cretegny <delphine.cretegny@free.fr>
2019-09-02 16:53:02 +0200

    home page advanced

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import { initSelect2, initFilter, initSelector } from '../plugins/init_select2';
 import { removeRow } from '../components/init_remove_row';
+// import { onScroll } from '../components/scroll';
 
 if (document.querySelector('.filter')) {
   initFilter();
@@ -17,3 +18,15 @@ if (document.querySelector('.js-example-basic-multiple')) {
 if (document.querySelector('.item-rm')) {
   removeRow();
 }
+
+window.onscroll = () => {
+  if(document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+    document.getElementById("i-animation").className = "recent-products-heading d-flex scroll";
+  } else document.getElementById("i-animation").className = "recent-products-heading d-flex";
+};
+
+// window.onscroll = () => {
+//   if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+//     document.getElementById("divider-animation").className = "featurette-divider divider-scroll";
+//   } else document.getElementById("divider-animation").className = "featurette-divider";
+// };
